d8b25ab6dbe7f2de4506924ca9a5fd01356cb03f
Justine Cacho <justinecacho@gmail.com>
2023-07-16 19:24:06 -0700

    adds comments

--- Grades/script.js
+++ Grades/script.js
@@ -3,6 +3,7 @@ var grades = "jim|25, sue|32, mary|34, ann|22, ted|28, frank|15, lisa|19, mike|3
 var students = grades.split(", ");
 var studentObjects = [];
 
+/* Iterate over the students array. Splits elements into a name and score, capitalize names, */
 for (var i = 0; i < students.length; i++) {
     var studentData = students[i].split("|");
     var name = studentData[0];
@@ -12,15 +13,18 @@ for (var i = 0; i < students.length; i++) {
     studentObjects.push(student);
 }
 
+/* Sorts students alphabetically */
 studentObjects.sort(function(a, b) {
     return a.name.localeCompare(b.name);
 });
 
+/* create a for loop that retrieves student names and their score */
 for (var i = 0; i < studentObjects.length; i++) {
     var student = studentObjects[i];
     document.write(student.name + " - " + student.score + "<br>");
 }
 
+/* Calculate lowest score, highest score, average score in "studentObjects array" */ 
 var lowestScore = studentObjects[0].score;
 var highestScore = studentObjects[studentObjects.length - 1].score;
 var sumScores = 0;
@@ -29,6 +33,7 @@ for (var i = 0; i < studentObjects.length; i++) {
 }
 var averageScore = sumScores / studentObjects.length;
 
+/* Outputs to the document */
 document.write("<br>Total students: " + studentObjects.length + "<br>");
 document.write("Lowest score: " + lowestScore + "<br>");
 document.write("Highest score: " + highestScore + "<br>");
